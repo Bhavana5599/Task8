2ea158dd54fae9a31c3e06257a80fe77964aa701
Bhavana5599 <p.bhavanareddy99@gmail.com>
2020-03-30 21:38:57 +0530

    commit

---
+++ ET8/src/main/java/ET8/ET8/App.java
@@ -0,0 +1,13 @@
+package ET8.ET8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
